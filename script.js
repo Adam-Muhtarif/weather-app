c17e753bea7c4661074995bbb8e6f1fe2395bdf2
Adam Muhtarif <muhtarif.dev.web@gmail.com>
2023-06-21 10:57:45 +0200

    done with it

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// write code below
+function initWeather() {
+  document.getElementById("location").textContent = "Unknown City";
+  document.querySelector(".desc").textContent = "No Information Available";
+  document.querySelector(".c").textContent = "Error";
+}
+initWeather();
+
+document.querySelector(".btn").addEventListener("click", () => {
+  console.log(document.getElementById("search").value);
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${
+      document.getElementById("search").value
+    }&appid=2765900c2cb0b5190b3a05f4dd4b8fb6`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      document.getElementById("location").textContent = data.name;
+      document.querySelector(".desc").textContent = data.weather[0].description;
+      document.querySelector(".c").textContent = data.main.temp;
+    })
+    .catch((reason) => {
+      console.log(reason);
+      initWeather();
+    });
+});
